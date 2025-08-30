75a26836bcaa15ea12f0f71a3ddcfab9490e83b5
AmzHoward <amzhoward@dev.com>
2025-08-30 16:42:17 +0800

    update: provider app, route, add base web routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Events\QueryExecuted;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!$this->app->runningInConsole()) {
+            // 非本機開發，強制https
+            if (!$this->app->environment([ 'local' ])) {
+                URL::forceScheme('https');
+            }
+        }
+
+        if (config('app.debug')) {
+            // 記錄query
+            DB::listen(function (QueryExecuted $query) {
+                logger()
+                ->channel('querylog')
+                ->debug("{$query->time} ms >> [{$query->connectionName}] {$query->sql}", $query->bindings);
+            });
+        }
+        // 記錄query時間過長
+        DB::whenQueryingForLongerThan(600, function (Connection $connection, QueryExecuted $event) {
+            logger()
+            ->channel('slowlog')
+            ->warning("{$event->time} ms >> [{$event->connectionName}] {$event->sql}", $event->bindings);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 根網址
+Route::get(
+    '/',
+    fn () => app()->isLocal() ? view('welcome') : response('Hello World')
+)->name('index');
+
+/**
+ * laravel若系統session整個失效，會導致沒辦法針對特定的guard做跳轉，
+ * 定義一個未知錯誤的路由，提供跳轉，避免跳轉至未定義路由而噴錯
+ * @see App\Exceptions\Handler unauthenticated
+ */
+Route::get(
+    '/woops',
+    fn () => response('Woops! Something wrong :( Please re-enter your url.')
+)->name('woops');
+
+// 運作狀態檢查
+Route::get(
+    '/healthcheck',
+    fn () => response('OK')
+)->name('healthcheck');
