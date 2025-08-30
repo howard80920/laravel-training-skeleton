470d468ef2e6c72e09e3150ff56b87f917dc2027
AmzHoward <amzhoward@dev.com>
2025-08-30 16:28:07 +0800

    add: config site

---
+++ config/site.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    // 語系
+    'locales' => [
+        'en' => 'English',
+        'zh_TW' => '繁體中文',
+        'zh_CN' => '簡體中文',
+        'ja' => '日本語',
+    ],
+
+    // 單一登入
+    'single_login' => [
+        'enable' => true,
+        'expire_days' => 14,
+    ],
+];
